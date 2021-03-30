a9670a74d83926a86b9283e1958a3097b39aa0ab
Kolaxaisz <kolaxaisz@gmail.com>
2021-03-30 12:50:49 +0200

    [create-a-news-app-with-vue-js-and-nuxt]
    - Finished Lesson 2.

--- vue/create-a-news-app-with-vue-js-and-nuxt/hackernuxt/pages/index.vue
+++ vue/create-a-news-app-with-vue-js-and-nuxt/hackernuxt/pages/index.vue
@@ -1,3 +1,10 @@
 <template>
-  <div>Hello World</div>
+  <div class="f1 code">
+    Hello World
+    <ul>
+      <li>One</li>
+      <li>Two</li>
+      <li>Three</li>
+    </ul>
+  </div>
 </template>
